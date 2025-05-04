66c59a12f70d8c3489003769a1fe2b705d23d969
marques-svnt <marques.svnt.002@gmail.com>
2025-05-03 23:18:04 -0300

    Criação das tasks de semaforo no led rgb e matriz, assim como configuração do modo noturno

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "pico/stdlib.h"
+#include "pico/bootrom.h"
 #include "hardware/gpio.h"
 #include "hardware/i2c.h"
 #include "lib/ssd1306.h"
@@ -6,105 +7,161 @@
 #include "FreeRTOS.h"
 #include "FreeRTOSConfig.h"
 #include "task.h"
+#include "pio.h"
+#include "display.h"
 #include <stdio.h>
 
-#define I2C_PORT i2c1
-#define I2C_SDA 14
-#define I2C_SCL 15
-#define endereco 0x3C
-
-#define led1 11
-#define led2 12
-
-void vBlinkLed1Task()
-{
-    gpio_init(led1);
-    gpio_set_dir(led1, GPIO_OUT);
-    while (true)
-    {
-        gpio_put(led1, true);
-        vTaskDelay(pdMS_TO_TICKS(250));
-        gpio_put(led1, false);
-        vTaskDelay(pdMS_TO_TICKS(1223));
-    }
-}
-
-void vBlinkLed2Task()
-{
-    gpio_init(led2);
-    gpio_set_dir(led2, GPIO_OUT);
-    while (true)
-    {
-        gpio_put(led2, true);
-        vTaskDelay(pdMS_TO_TICKS(500));
-        gpio_put(led2, false);
-        vTaskDelay(pdMS_TO_TICKS(2224));
-    }
-}
-
-void vDisplay3Task()
-{
-    // I2C Initialisation. Using it at 400Khz.
-    i2c_init(I2C_PORT, 400 * 1000);
-
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
-    gpio_pull_up(I2C_SDA);                                        // Pull up the data line
-    gpio_pull_up(I2C_SCL);                                        // Pull up the clock line
-    ssd1306_t ssd;                                                // Inicializa a estrutura do display
-    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
-    ssd1306_config(&ssd);                                         // Configura o display
-    ssd1306_send_data(&ssd);                                      // Envia os dados para o display
-    // Limpa o display. O display inicia com todos os pixels apagados.
-    ssd1306_fill(&ssd, false);
-    ssd1306_send_data(&ssd);
-
-    char str_y[5]; // Buffer para armazenar a string
-    int contador = 0;
-    bool cor = true;
-    while (true)
-    {
-        sprintf(str_y, "%d", contador); // Converte em string
-        contador++;                     // Incrementa o contador
-        ssd1306_fill(&ssd, !cor);                          // Limpa o display
-        ssd1306_rect(&ssd, 3, 3, 122, 60, cor, !cor);      // Desenha um retângulo
-        ssd1306_line(&ssd, 3, 25, 123, 25, cor);           // Desenha uma linha
-        ssd1306_line(&ssd, 3, 37, 123, 37, cor);           // Desenha uma linha
-        ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 6); // Desenha uma string
-        ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 16);  // Desenha uma string
-        ssd1306_draw_string(&ssd, "  FreeRTOS", 10, 28); // Desenha uma string
-        ssd1306_draw_string(&ssd, "Contador  LEDs", 10, 41);    // Desenha uma string
-        ssd1306_draw_string(&ssd, str_y, 40, 52);          // Desenha uma string
-        ssd1306_send_data(&ssd);                           // Atualiza o display
-        sleep_ms(735);
-    }
-}
-
-// Trecho para modo BOOTSEL com botão B
-#include "pico/bootrom.h"
+#define botaoA 5
 #define botaoB 6
+#define ledVerde 11
+#define ledAzul 12
+#define ledVermelho 13
+
+static volatile uint32_t last_time_A = 0; // Armazena o tempo do último evento (em microssegundos)
+static volatile uint32_t last_time_B = 0;
+volatile bool modoNoturno = false;
+
+bool debounce(volatile uint32_t *last_time, uint32_t debounce_time)
+{
+    uint32_t current_time = to_us_since_boot(get_absolute_time());
+    if (current_time - *last_time > debounce_time)
+    {
+        *last_time = current_time;
+        return true;
+    }
+    return false;
+}
+
+// Função de interrupção com debouncing
 void gpio_irq_handler(uint gpio, uint32_t events)
 {
-    reset_usb_boot(0, 0);
+    uint32_t current_time = to_us_since_boot(get_absolute_time());
+
+    // Alternar o modo noturno
+    if (gpio == botaoA && debounce(&last_time_A, 300000))
+    {
+        last_time_A = current_time;
+        modoNoturno = !modoNoturno;
+    }
+}
+
+void vSemaforoNormal()
+{
+    while (true)
+    {
+        gpio_put(ledVerde, true); // Garantir que o verde vai ser ativado assim que entrar no laço
+        gpio_put(ledVermelho, false);
+        if (!modoNoturno)
+        {
+            gpio_put(ledVerde, true);
+            vTaskDelay(pdMS_TO_TICKS(1000));
+            if (modoNoturno)
+                continue;
+
+            gpio_put(ledVermelho, true);
+            vTaskDelay(pdMS_TO_TICKS(1000));
+            if (modoNoturno)
+                continue;
+
+            gpio_put(ledVerde, false);
+            vTaskDelay(pdMS_TO_TICKS(1000));
+            if (modoNoturno)
+                continue;
+
+            gpio_put(ledVermelho, false);
+        }
+        else
+        {
+            gpio_put(ledVerde, true);
+            gpio_put(ledVermelho, true);
+            vTaskDelay(pdMS_TO_TICKS(1500));
+            if (!modoNoturno)
+                continue;
+
+            gpio_put(ledVerde, false);
+            gpio_put(ledVermelho, false);
+            vTaskDelay(pdMS_TO_TICKS(1500));
+        }
+    }
+}
+
+void vMatrizLeds()
+{
+    while (true)
+    {
+        if (!modoNoturno)
+        {
+            set_one_led(0, 0, 20, 0);
+            vTaskDelay(pdMS_TO_TICKS(1000));
+            if (modoNoturno)
+                continue;
+
+            set_one_led(1, 20, 20, 0);
+            vTaskDelay(pdMS_TO_TICKS(1000));
+            if (modoNoturno)
+                continue;
+
+            set_one_led(2, 20, 0, 0);
+            vTaskDelay(pdMS_TO_TICKS(1000));
+        }
+        else
+        {
+            set_one_led(1, 20, 20, 0);
+            vTaskDelay(pdMS_TO_TICKS(1500));
+            if (!modoNoturno)
+                continue;
+            set_one_led(4, 0, 0, 0);
+            vTaskDelay(pdMS_TO_TICKS(1500));
+        }
+    }
+}
+
+void vBuzzer()
+{
+    while (true)
+    {
+        vTaskDelay(pdMS_TO_TICKS(1));
+    }
+}
+
+void vDisplay()
+{
+    while (true)
+    {
+        vTaskDelay(pdMS_TO_TICKS(1));
+    }
 }
 
 int main()
 {
-    // Para ser utilizado o modo BOOTSEL com botão B
-    gpio_init(botaoB);
-    gpio_set_dir(botaoB, GPIO_IN);
-    gpio_pull_up(botaoB);
-    gpio_set_irq_enabled_with_callback(botaoB, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
-    // Fim do trecho para modo BOOTSEL com botão B
+    gpio_init(botaoA);
+    gpio_set_dir(botaoA, GPIO_IN);
+    gpio_pull_up(botaoA);
+
+    gpio_set_irq_enabled_with_callback(botaoA, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+
+    gpio_init(ledAzul);
+    gpio_set_dir(ledAzul, GPIO_OUT);
+
+    gpio_init(ledVerde);
+    gpio_set_dir(ledVerde, GPIO_OUT);
+
+    gpio_init(ledVermelho);
+    gpio_set_dir(ledVermelho, GPIO_OUT);
 
     stdio_init_all();
+    initializePio();
+    initI2C();
 
-    xTaskCreate(vBlinkLed1Task, "Blink Task Led1", configMINIMAL_STACK_SIZE,
-         NULL, tskIDLE_PRIORITY, NULL);
-    xTaskCreate(vBlinkLed2Task, "Blink Task Led2", configMINIMAL_STACK_SIZE, 
-        NULL, tskIDLE_PRIORITY, NULL);
-    xTaskCreate(vDisplay3Task, "Cont Task Disp3", configMINIMAL_STACK_SIZE, 
-        NULL, tskIDLE_PRIORITY, NULL);
+    xTaskCreate(vSemaforoNormal, "Semaforo Normal", configMINIMAL_STACK_SIZE,
+                NULL, tskIDLE_PRIORITY, NULL);
+    xTaskCreate(vMatrizLeds, "Matriz Leds", configMINIMAL_STACK_SIZE,
+                NULL, tskIDLE_PRIORITY, NULL);
+    xTaskCreate(vBuzzer, "Buzzer", configMINIMAL_STACK_SIZE,
+                NULL, tskIDLE_PRIORITY, NULL);
+    xTaskCreate(vDisplay, "Display", configMINIMAL_STACK_SIZE,
+                NULL, tskIDLE_PRIORITY, NULL);
+
     vTaskStartScheduler();
     panic_unsupported();
 }
